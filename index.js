aa73b29bf4604755662bbf80f7c6635f5b2ab688
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-12 15:38:00 +0530

    reverse string

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function titileCase(str) {
-  let arrOfWord = str.toLowerCase().split(" ");
-  for (let i = 0; i < arrOfWord.length; i++) {
-    arrOfWord[i] = arrOfWord[i][0].toUpperCase() + arrOfWord[i].slice(1);
+function reverseString(str) {
+  let reverse = "";
+  for (let i = 0; i < str.length; i++) {
+    reverse = str[i] + reverse;
   }
-  return arrOfWord.join(" ");
+  return reverse;
 }
-console.log(titileCase("the laZY FOX is well"));
+
+console.log(reverseString("hello"));
+
+function reverseStringBack(str) {
+  let reverse = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+  console.log(reverse);
+}
+
+reverseStringBack("hello world");
