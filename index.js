b91e55214270cc8d5ae976d424b6d16ceeb1b168
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-13 16:07:23 +0530

    missing no.

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-function liked(arr) {
-  const n = arr.length;
-  switch (n) {
-    case 0:
-      console.log(`No One Likes This`);
-      break;
-    case 1:
-      console.log(`${arr[0]} likes this`);
-      break;
-    case 2:
-      console.log(`${arr[0]} and ${arr[1]} like this`);
-      break;
-    case 3:
-      console.log(`${arr[0]} , ${arr[1]} and ${n - 2} other like this`);
-      break;
-    default:
-      console.log(`${arr[0]} , ${arr[1]} and ${n - 2} others like this`);
-  }
+function findMissing(arr) {
+  const n = arr.length + 1;
+  const expectedSum = (n * (n + 1)) / 2;
+  const actualSum = arr.reduce((cur, acc) => cur + acc, 0);
+  return expectedSum - actualSum;
 }
 
-liked(["lufy", "sanji"]);
-liked(["lufy"]);
-liked(["lufy", "sanji", "nami"]);
-liked(["lufy", "sanji", "nami", "zoro"]);
+console.log(findMissing([1, 2, 4, 5, 6]));
