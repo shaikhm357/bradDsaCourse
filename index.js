ec309133d676e867df5d424d34a9cceecd6cd88e
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-13 11:46:53 +0530

    fizz buzz

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-function removeDuplicates(arr) {
-  const unique = [];
+function fizzBuzz(arr) {
   for (let i = 0; i < arr.length; i++) {
-    if (!unique.includes(arr[i])) {
-      unique.push(arr[i]);
+    if (arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+      console.log("fizzBuzz");
+      continue;
+    }
+    if (arr[i] % 3 === 0) {
+      console.log("fizz");
+    } else if (arr[i] % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(arr[i]);
     }
   }
-  console.log(unique);
 }
 
-removeDuplicates([2, 4, 4, 5, 5, 6, 6, 6, 6]);
+fizzBuzz([0, 6, 5, 4, 8, 9, 15, 8, 11, 18, 30, 20, 25]);
