9075c89ef6d747571dac781ee92dde37916abc38
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-16 23:03:51 +0530

    sum of even square

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const doubleNumbers = numbers.map((index, num, arr) => {
-  // console.log(index);
-  // console.log(arr);
-  return num * 2;
-});
+const sumOfEvenSquare = numbers
+  .filter((num) => num % 2 === 0)
+  .map((num) => num * num)
+  .reduce((acc, cur) => acc + cur, 0);
 
-console.log(doubleNumbers);
-
-const evennum = numbers.filter((num) => num > 2);
-console.log(evennum);
-
-const accumulate = numbers.reduce((acc, cur) => acc + cur, 0);
-console.log(accumulate);
-
-// numbers.forEach((num) => console.log(num));
-
-const foundNum = numbers.find((num) => num > 2);
-console.log(foundNum);
-
-const hasEvenNumber = numbers.some((num) => num > 3);
-console.log(hasEvenNumber);
-
-const allNumGeaterThanZero = numbers.every((num) => num > 0);
-console.log(allNumGeaterThanZero);
+console.log(sumOfEvenSquare);
