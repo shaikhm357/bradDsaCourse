1dbd978fdf2146d4a200d8c461119a8a6815782c
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-18 19:55:19 +0530

    merge sorted array

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-function validPassword(password) {
-  const isLengthValid = password.length >= 8;
+function mergeSorted(arr1, arr2) {
+  let i = arr1.length - 1;
+  let j = arr2.length - 1;
+  let k = arr1.length + arr2.length - 1;
 
-  const hasUpperCase = password
-    .split("")
-    .some((char) => char === char.toUpperCase() && char != char.toLowerCase());
-
-  const hasLowerCase = password
-    .split("")
-    .some((char) => char === char.toLowerCase() && char != char.toUpperCase());
-
-  const hasDigit = password.split("").some((char) => !isNaN(parseInt(char)));
-
-  return isLengthValid && hasUpperCase && hasLowerCase && hasDigit;
+  while (j >= 0) {
+    if (i >= 0 && arr1[i] > arr2[j]) {
+      arr1[k--] = arr1[i--];
+    } else {
+      arr1[k--] = arr2[j--];
+    }
+  }
+  return arr1;
 }
 
-console.log(validPassword("abcAAdll1l"));
-console.log(validPassword("abc1238888888"));
-console.log(validPassword("AAAA34343434a"));
-console.log(validPassword("abc123ABC"));
+console.log(mergeSorted([1, 2, 4, 5], [3, 6]));
+console.log(mergeSorted([4, 5], [3, 6]));
+console.log(mergeSorted([1, 5], [3, 6, 7]));
+console.log(mergeSorted([1, 2], [3, 4, 6]));
