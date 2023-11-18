4a1c713c64f6f508894bab86174aa1fb401bb26e
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-18 12:15:21 +0530

    car mileage details query

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-function isValidIpv4(ip) {
-  let octates = ip.split(".");
-  if (octates.length !== 4) {
-    return false;
-  }
-  return octates.every((octate) => {
-    let num = parseInt(octate);
-    return num >= 0 && num <= 255 && octate === num.toString();
-  });
+const cars = [
+  { make: "Toyota", model: "Camry", year: 2020, mileage: 30800.22 },
+  { make: "Honda", model: "Civic", year: 2019, mileage: 32000.12 },
+  { make: "Chevrolet", model: "Impala", year: 2021, mileage: 17500 },
+  { make: "Audi", model: "R8", year: 2020, mileage: 13000 },
+  { make: "Tesla", model: "Model 3", year: 2018, mileage: 50000 },
+];
+
+console.log(calculateCarDetails(cars));
+
+function calculateCarDetails(cars) {
+  const totalMileage = cars.reduce((mile, car) => mile + car.mileage, 0);
+  const averageMileage = totalMileage / cars.length;
+
+  const HighestMile = cars.reduce(
+    (highest, car) => (car.mileage > highest.mileage ? car : highest),
+    cars[0]
+  );
+
+  const LowestMile = cars.reduce(
+    (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+    cars[0]
+  );
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    totalMileage: parseFloat(totalMileage.toFixed(2)),
+    highestMileage: HighestMile,
+    lowestMileage: LowestMile,
+  };
 }
-console.log(isValidIpv4("1.2.3.4"));
-console.log(isValidIpv4("123.244.140.4.9"));
-console.log(isValidIpv4("0.2.300.4"));
-console.log(isValidIpv4("0.2.255.4"));
