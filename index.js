d703064e61c72f99e88e65a77f18860a4b7cafca
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-18 20:10:25 +0530

    count down

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-function mergeSorted(arr1, arr2) {
-  let i = arr1.length - 1;
-  let j = arr2.length - 1;
-  let k = arr1.length + arr2.length - 1;
-
-  while (j >= 0) {
-    if (i >= 0 && arr1[i] > arr2[j]) {
-      arr1[k--] = arr1[i--];
-    } else {
-      arr1[k--] = arr2[j--];
-    }
+function countDown(num) {
+  // base case
+  if (num <= 0) {
+    console.log("All Done");
+    return;
   }
-  return arr1;
+
+  // recursive call
+  console.log(num);
+  countDown(num - 1);
 }
 
-console.log(mergeSorted([1, 2, 4, 5], [3, 6]));
-console.log(mergeSorted([4, 5], [3, 6]));
-console.log(mergeSorted([1, 5], [3, 6, 7]));
-console.log(mergeSorted([1, 2], [3, 4, 6]));
+countDown(10);
