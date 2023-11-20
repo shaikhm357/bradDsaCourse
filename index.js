2310fef8af01624d9de192f529d2905bc84884cf
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-20 12:00:24 +0530

    fact recur

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function findfibo(num) {
-  if (num < 2) {
-    return num;
+function fact(num) {
+  if (num === 1) {
+    return 1;
   }
-  return findfibo(num - 1) + findfibo(num - 2);
+  return fact(num - 1) * num;
 }
 
-console.log(findfibo(40));
+console.log(fact(4));
