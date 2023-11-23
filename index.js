8929c31b140b3b2cab1131cbdd861fec2e6ec0d4
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-23 08:06:05 +0530

    mas sub arry sum

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-function flattenArray(arr) {
-  let newArr = [];
-  for (const el of arr) {
-    if (Array.isArray(el)) {
-      newArr = newArr.concat(flattenArray(el));
-    } else {
-      newArr.push(el);
+function maxSubarraySum(arr, k) {
+  let maxSum = 0;
+  for (let i = 0; i <= arr.length - k; i++) {
+    let sum = 0;
+    for (let j = i; j < i + k; j++) {
+      sum += arr[j];
     }
+    maxSum = Math.max(sum, maxSum);
   }
-  return newArr;
+  return maxSum;
 }
-console.log(flattenArray([1, [2, 3], [4, 5, [6]]]));
+const arr1 = [2, 5, 3, 1, 11, 7, 6, 4];
+const k1 = 3;
+console.log(maxSubarraySum(arr1, k1));
