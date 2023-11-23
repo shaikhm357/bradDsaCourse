7df22f2d010ef52fc25a0f591fc81e3f32162cf0
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-23 08:17:05 +0530

    sliding window technique is refactor for max sum of sub arr

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function maxSubarraySum(arr, k) {
   let maxSum = 0;
-  for (let i = 0; i <= arr.length - k; i++) {
-    let sum = 0;
-    for (let j = i; j < i + k; j++) {
-      sum += arr[j];
-    }
-    maxSum = Math.max(sum, maxSum);
+  let currentSum = 0;
+
+  for (let i = 0; i < k; i++) {
+    maxSum += arr[i];
+  }
+  currentSum = maxSum;
+  for (let i = k; i < arr.length; i++) {
+    currentSum = currentSum - arr[i - k] + arr[i];
+    maxSum = Math.max(currentSum, maxSum);
   }
   return maxSum;
 }
