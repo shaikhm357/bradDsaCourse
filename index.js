967a5201bcee48537e6c01ecba513e7336c36c09
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-24 22:17:16 +0530

    two some

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-function symmentricDiff(arr1, arr2) {
-  const set1 = new Set(arr1);
-  const set2 = new Set(arr2);
-  const result = [];
-  for (const el of arr1) {
-    if (!set2.has(el)) {
-      result.push(el);
+function twoSum(arr, target) {
+  const numSet = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    let complement = target - arr[i];
+    if (numSet.has(complement)) {
+      return [arr.indexOf(complement), i];
     }
+    numSet.add(arr[i]);
   }
-  for (const el of arr2) {
-    if (!set1.has(el)) {
-      result.push(el);
-    }
-  }
-  return result;
 }
 
-console.log(symmentricDiff([1, 2, 3], [2, 3, 4]));
+console.log(twoSum([2, 3, 4, 7, 5, 7], 11));
+console.log(twoSum([2, 7, 11, 15], 17));
