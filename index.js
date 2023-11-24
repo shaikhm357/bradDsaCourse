1bd2bd134dc0c88fbb0f19883df8dfc129e83d5a
Shaikh Mahboob <shaikhm357@gmail.com>
2023-11-25 00:15:23 +0530

    hash table part 1

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-function inSequence(arr) {
-  const numSet = new Set(arr);
-  let longSeq = 0;
-  for (const num of numSet) {
-    if (!numSet.has(num - 1)) {
-      let currNum = num;
-      let currSeq = 1;
-      while (numSet.has(currNum + 1)) {
-        currNum++;
-        currSeq++;
+class HashTable {
+  constructor(limit = 14) {
+    this.limit = limit;
+    this.storage = [];
+  }
+  _hash(key, max) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % max;
+  }
+  printTable() {
+    for (let i = 0; i < this.storage.length; i++) {
+      if (this.storage[i] !== undefined) {
+        console.log(`${i} : ${JSON.stringify(this.storage[i])}`);
+      } else {
+        console.log(`${i} : "empty"`);
       }
-      longSeq = Math.max(longSeq, currSeq);
     }
   }
-  return longSeq;
+  set(key, value) {
+    const index = this._hash(key, this.limit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]];
+    } else {
+      let inserted = false;
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i][i] = value;
+          inserted = true;
+        }
+      }
+      if (inserted === false) {
+        this.storage[index].push([key, value]);
+      }
+    }
+  }
 }
 
-console.log(inSequence([100, 4, 200, 1, 2, 3]));
+let table = new HashTable(15);
+table.set("John", "720-816-601");
+table.set("Mahboob", "720-816-601");
+
+table.printTable();
