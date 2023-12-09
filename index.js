39eeef0a2334ed142b9e9f36f111a7759755587e
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-09 07:34:54 +0530

    buuble sort 1

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
+  let swapped = false;
+  while (!swapped) {
+    swapped = true;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = false;
       }
     }
   }
-  return arr;
+  return arr
 }
 
 console.log(bubbleSort([5, 4, 2, 1]));
