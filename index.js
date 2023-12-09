2522e6c7b650ae343f26d80113753b15037dd01d
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-09 07:54:44 +0530

    section sort

--- index.js
+++ index.js
@@ -11,22 +11,19 @@
 //   return arr;
 // }
 
-function bubbleSort(arr) {
-  let swapped = false;
-  while (!swapped) {
-    swapped = true;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        let temp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = temp;
-        swapped = false;
+function selctionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if(arr[i]>arr[j]){
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
       }
     }
   }
-  return arr
+  return arr;
 }
 
-console.log(bubbleSort([5, 4, 2, 1]));
-console.log(bubbleSort([4, 5, 2, 1]));
-console.log(bubbleSort([3, 4, 2, 1]));
+console.log(selctionSort([5, 4, 2, 1]));
+console.log(selctionSort([4, 5, 2, 1]));
+console.log(selctionSort([3, 4, 2, 1]));
