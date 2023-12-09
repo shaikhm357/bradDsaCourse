495e739af746b0315556a77424a64139e23d2434
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-09 11:51:37 +0530

    mege sort

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let currVal = arr[i]
-    let j = i - 1;
-    while (j >= 0 && arr[j] > currVal) {
-      arr[j + 1] = arr[j];
-      j--;
-    }
-    arr[j + 1] = currVal;
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
   }
-  return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
 }
 
-console.log(insertionSort([5, 4, 2, 1]));
-console.log(insertionSort([4, 5, 2, 1]));
-console.log(insertionSort([3, 4, 2, 1]));
+function merge(left, right) {
+  const merged = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      merged.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      merged.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+  return merged.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+console.log(mergeSort([5, 4, 2, 1]));
+console.log(mergeSort([4, 5, 2, 1]));
+console.log(mergeSort([3, 4, 2, 1]));
