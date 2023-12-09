2fb8d0480341ab9d87cc625d0f18d32000b247a6
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-09 11:37:03 +0530

    insertion sort

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-// function bubbleSort(arr) {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = 0; j < arr.length - i; j++) {
-//       if (arr[j] > arr[j + 1]) {
-//         let temp = arr[j];
-//         arr[j] = arr[j + 1];
-//         arr[j + 1] = temp;
-//       }
-//     }
-//   }
-//   return arr;
-// }
-
-function selctionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if(arr[i]>arr[j]){
-        let temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-      }
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currVal = arr[i]
+    let j = i - 1;
+    while (j >= 0 && arr[j] > currVal) {
+      arr[j + 1] = arr[j];
+      j--;
     }
+    arr[j + 1] = currVal;
   }
   return arr;
 }
 
-console.log(selctionSort([5, 4, 2, 1]));
-console.log(selctionSort([4, 5, 2, 1]));
-console.log(selctionSort([3, 4, 2, 1]));
+console.log(insertionSort([5, 4, 2, 1]));
+console.log(insertionSort([4, 5, 2, 1]));
+console.log(insertionSort([3, 4, 2, 1]));
