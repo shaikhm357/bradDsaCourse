37c04ad829541afb9c865e43727b764b3f338cae
Shaikh Mahboob <shaikhm357@gmail.com>
2023-12-14 08:20:33 +0530

    depth first traversal

--- index.js
+++ index.js
@@ -6,19 +6,41 @@ class TreeNode {
   }
 }
 
-let a = new TreeNode("a");
+let root = new TreeNode("a");
 let b = new TreeNode("b");
 let c = new TreeNode("c");
 let d = new TreeNode("d");
 let e = new TreeNode("e");
 let f = new TreeNode("f");
 
-a.left = b;
-a.right = c;
+root.left = b;
+root.right = c;
 
 b.left = d;
 b.right = e;
 
-d.right = f;
+c.left = f;
 
-console.log(b);
+// console.log(root);
+
+console.log((function depth_1st_traversal(root) {
+  if (!root) return [];
+
+  const result = [];
+  const stack = [];
+  stack.push(root);
+  while (stack.length > 0) {
+    let current = stack.pop();
+
+    result.push(current.value);
+
+    if (current.right) {
+      stack.push(current.right);
+    }
+
+    if (current.left) {
+      stack.push(current.left);
+    }
+  }
+  return result;
+})(root))
